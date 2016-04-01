92f22754e7271c0feff1fafc4c39e3de5a5edc3b
pp3n <pp3n@protonmail.ch>
2016-04-01 17:29:06 -0400

    more rails

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "Hello World!"
+  	render text: "Hola, mundo!"
   end
+
+  def goodbye
+  	render text: "Goodbye world!"
 end
